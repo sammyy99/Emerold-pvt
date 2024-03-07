5214665670f777cf811517f1c37c182a67df737d
sammyy99 <samarthbhardwaj1999@gmail.com>
2024-03-08 01:54:50 +0530

    Header design final

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import XIcon from "@mui/icons-material/X";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -8,30 +12,59 @@ const Header = () => {
   };
 
   return (
-    <div className="flex md:flex-row flex-row-reverse py-3 justify-between md:items-center px-4 md:px-20 bg-gradient-to-r from-emerald-100 to bg-emerald-400 font-serif">
-      <div className="text-2xl md:text-4xl">Emerold</div>
+    <div className="fixed w-full shadow-md font-serif md:flex md:justify-between grid grid-cols-2 py-4 md:py-5 px-4 md:px-10 text-white bg-neutral-800">
 
-      <div className="">
-        <button
-          onClick={() => {
-            handleMenu();
-          }}
-          className="md:hidden"
-        >
-          <img className="w-8" alt="" src="menu-icon.svg"></img>
-        </button>
+      <div className="col-span-1 text-3xl font-semibold flex space-x-1 items-center"><div>Emerold</div><img className="w-10" alt="" src="logo.svg"></img></div>
 
-        <ul
-          className={`md:flex md:space-x-6 text-lg transition-transform duration-500 ease-in-out transform
-          ${showMenu ? "mt-2 pl-1" : "hidden"}
-          `}
-        >
-          <li>Home</li>
-          <li>About</li>
-          <li>Products</li>
-          <li>Services</li>
-          <li>Contact us</li>
-        </ul>
+      <div
+        onClick={() => {
+          handleMenu();
+        }}
+        className="md:hidden col-span-1 flex justify-end"
+      >
+        <MenuIcon fontSize="large" />
+      </div>
+
+      <nav
+        className={`md:flex md:space-x-12 text-xl transition-all duration-500 ease-in-out ${
+          showMenu
+            ? "col-span-2 text-center pt-8 bg-neutral-800 text-white rounded-t-xl"
+            : "hidden"
+        }`}
+      >
+        <div className="py-1 md:py-auto hover:text-emerald-500 hover:cursor-pointer">
+          Home
+        </div>
+        <div className="py-1 md:py-auto hover:text-emerald-500 hover:cursor-pointer">
+          About
+        </div>
+        <div className="py-1 md:py-auto hover:text-emerald-500 hover:cursor-pointer">
+          Products
+        </div>
+        <div className="py-1 md:py-auto hover:text-emerald-500 hover:cursor-pointer">
+          Services
+        </div>
+        <div className="pt-1 md:py-auto hover:text-emerald-500 hover:cursor-pointer">
+          Contact
+        </div>
+      </nav>
+
+      <div
+        className={`md:flex space-x-3 ${
+          showMenu
+            ? "col-span-2 flex justify-center py-6 bg-neutral-800 text-white rounded-b-xl"
+            : "hidden"
+        }`}
+      >
+        <div className=" hover:text-emerald-500  hover:cursor-pointer">
+          <XIcon />
+        </div>
+        <div className=" hover:text-emerald-500  hover:cursor-pointer">
+          <FacebookIcon />
+        </div>
+        <div className=" hover:text-emerald-500  hover:cursor-pointer">
+          <LinkedInIcon />
+        </div>
       </div>
     </div>
   );
